d7087e35d366dabd286c45aebe7d8c7fce28457a
Derrick Furtado <derrick.furtado10@gmail.com>
2024-03-08 12:04:45 -0700

    almost done

--- runtime.js
+++ runtime.js
@@ -48,15 +48,80 @@ const extraLargeArray = getSizedArray(100000);
 // Try it with first function
 perf.start();                     // Starts timer
 doublerAppend(extraLargeArray);
-let resultsAppend = perf.stop();  // Stops timer and save time results
+let resultsAppendXL = perf.stop();  // Stops timer and save time results
 
 
 // Try it with second function
 perf.start();
 doublerInsert(extraLargeArray);
-let resultsInsert = perf.stop();
+let resultsInsertXL = perf.stop();
+
+//Large ==========================
+perf.start();                     
+doublerAppend(largeArray);
+let resultsAppendL = perf.stop();
+
+perf.start();
+doublerInsert(largeArray);
+let resultsInsertL = perf.stop();
+
+//Medium ==========================
+perf.start();                     
+doublerAppend(mediumArray);
+let resultsAppendM = perf.stop();
+
+perf.start();
+doublerInsert(mediumArray);
+let resultsInsertM = perf.stop();
+
+//Small =========================
+perf.start();                     
+doublerAppend(smallArray);
+let resultsAppendS = perf.stop();
+
+perf.start();
+doublerInsert(smallArray);
+let resultsInsertS = perf.stop();
+
+//Tiny ==========================
+perf.start();                     
+doublerAppend(tinyArray);
+let resultsAppendXS = perf.stop();
+
+perf.start();
+doublerInsert(tinyArray);
+let resultsInsertXS = perf.stop();
+
+// console.log("========================")
+
+// console.log('Results for the tinyArray');
+// console.log("append", resultsAppendXS.preciseWords);
+// console.log("insert", resultsInsertXS.preciseWords);
+
+console.log("========================")
+
+console.log('Results for the smallArray');
+console.log("append", resultsAppendS.preciseWords);
+console.log("insert", resultsInsertS.preciseWords);
 
 
-console.log('Results for the extraLargeArray');
-console.log("insert", resultsInsert.preciseWords);
-console.log("append", resultsAppend.preciseWords);
+// console.log("========================")
+
+
+// console.log('Results for the mediumArray');
+// console.log("append", resultsAppendM.preciseWords);
+// console.log("insert", resultsInsertM.preciseWords);
+
+// console.log("========================")
+
+// console.log('Results for the largeArray');
+// console.log("append", resultsAppendL.preciseWords);
+// console.log("insert", resultsInsertL.preciseWords);
+
+// console.log("========================")
+
+// console.log('Results for the extraLargeArray');
+// console.log("append", resultsAppendXL.preciseWords);
+// console.log("insert", resultsInsertXL.preciseWords);
+
+// console.log("========================")
---
+++ write-code.js
@@ -0,0 +1,120 @@
+
+const addToZero = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        let num1 = arr[i]
+        for (let j = 0; j < arr.length; j++) {
+            let num2 = arr[j]
+            if (num1 + num2 === 0) {
+                return console.log("true")
+            }
+        }
+    } return console.log("false")
+}
+
+// RUNTIME IS = O(n^2)
+// Space complexity is = O(1)
+
+console.log("=======================")
+
+console.log("Add to Zero results")
+
+
+
+addToZero([1]);
+// -> False
+
+addToZero([1, 2, 3]);
+// -> False
+
+addToZero([1, 2, 3, -2]);
+// -> True
+
+addToZero([1, 4, 3, -2, 5, 3, -5]);
+console.log("=======================")
+
+console.log("Has Unique Chars results")
+
+
+
+
+const hasUniqueChars = (str) => {
+    let charSet = new Set(str)
+    if (charSet.size === str.length) {
+        console.log("true")
+    } else {
+        console.log("false")
+    }
+}
+
+
+// RUNTIME IS = O(n)
+// Space complexity is = O(n)
+
+
+hasUniqueChars("Monday");
+// -> True
+
+hasUniqueChars("Moonday");
+// -> False
+
+hasUniqueChars("aaaaa");
+
+hasUniqueChars("abcdefghijklmnopqrstuvwxyz0123456789");
+
+
+console.log("=======================")
+
+console.log("Is Pangram results")
+
+
+const isPangram = (str) => {
+    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    let compare = []
+    str = str.toLowerCase()
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < alphabet.length; j++) {
+            if (str[i] === alphabet[j]) {
+                compare.push(str[i])
+            }
+        }
+    }
+    let cleanCompare = new Set(compare)
+    if (cleanCompare.size === alphabet.length) {
+        console.log("true")
+    } else {
+        console.log("false")
+    }
+}
+
+// RUNTIME IS = O(n)
+// Space complexity is = O(n)
+
+
+
+isPangram("The quick brown fox jumps over the lazy dog!");
+// -> True
+
+isPangram("I like cats, but not mice");
+// -> False
+
+isPangram("abcdefghijklmnopqrstuvwxyz");
+
+isPangram("abcdefghijklmnopqrstuvwxyz0123456789")
+
+
+
+console.log("=======================")
+
+// const findLongestWord = {
+
+// }
+
+// // RUNTIME IS = 
+// // Space complexity is = 
+
+
+
+// findLongestWord(["hi", "hello"]);
+// // -> 5
+
+console.log("=======================")
